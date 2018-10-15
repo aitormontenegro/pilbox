29d81d9586df8fca061b70853ba07570b9c699dd
Aitor <aitor@bodas.net>
2018-10-15 17:55:39 +0200

    commitando un cambio - lun oct 15 17:55:39 CEST 2018

--- pilbox/app.py
+++ pilbox/app.py
@@ -320,7 +320,7 @@ class ImageHandler(tornado.web.RequestHandler):
         customfile = self.get_argument("file")
         if customfile:
             return
-        elif:
+        else:
             if not url and not customfile:
                 raise errors.UrlError("Missing url")
             elif url.startswith("http://") or url.startswith("https://"):

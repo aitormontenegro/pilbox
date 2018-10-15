d914e37f544a2eb06218c6da4403d2f07096390d
Aitor <aitor@bodas.net>
2018-10-15 12:54:23 +0200

    commitando un cambio - lun oct 15 12:54:23 CEST 2018

--- pilbox/app.py
+++ pilbox/app.py
@@ -200,7 +200,7 @@ class ImageHandler(tornado.web.RequestHandler):
         client = tornado.httpclient.AsyncHTTPClient(
             max_clients=self.settings.get("max_requests"))
         try:
-            raise errors.OperationError("File not implemented, yet"+url)
+            raise errors.OperationError("File not implemented, yet"+type(url))
             if url.startswith("http://") or url.startswith("https://"):
                 resp = yield client.fetch(
                     url,

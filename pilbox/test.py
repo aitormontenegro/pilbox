4080fb853b2abbec91dfb6db54564e2a8d231cd5
Aitor <aitor@bodas.net>
2018-10-16 17:31:41 +0200

    commitando un cambio - mar oct 16 17:31:41 CEST 2018

--- pilbox/test.py
+++ pilbox/test.py
@@ -6,7 +6,7 @@ from pilbox.image import Image
 def main():
     print("hola mundo")
 
-    Image(Image.open("/tmp/logo-vpn-01.png"))
+    img = Image("/tmp/logo-vpn-01.png")
 
 if __name__ == "__main__":
     main()
